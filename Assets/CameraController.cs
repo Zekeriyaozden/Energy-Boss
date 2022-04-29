08e6f2f0e9d63f254b071ed815d01899649b52e8
Zekeriyaozden <zekeriyaozden@icloud.com>
2022-04-29 13:36:57 +0300

    Anims

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+    public Vector3 distance;
+    void Start()
+    {
+        distance = Player.transform.position - gameObject.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        gameObject.transform.position = Player.transform.position - distance;
+    }
+}
