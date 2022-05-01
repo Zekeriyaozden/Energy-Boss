eca2611bfc13aa751400badf2f2aa8588a019137
Zekeriyaozden <zekeriyaozden@icloud.com>
2022-05-02 01:25:39 +0300

    StackMechanics

---
+++ Assets/Scripts/GameManeger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManeger : MonoBehaviour
+{
+    public Stack<GameObject> moneyStack = new Stack<GameObject>();
+    public int collectSize;
+    public GameObject collectObj;
+    public GameObject objTemp;
+    void Start()
+    {
+        collectSize = 0;
+        
+    }
+    void Update()
+    {
+        collectSize = collectObj.transform.childCount;
+    }
+
+    public GameObject PopStack()
+    {
+        if (moneyStack.Count != 0)
+        {
+            return moneyStack.Pop();   
+        }
+        else
+        {
+            return gameObject;
+        }
+    }
+
+    public void PushStack(GameObject money)
+    {
+        Debug.Log("Pusshing " + money.name);
+        moneyStack.Push(money);
+    }
+    
+}
---
+++ Assets/Scripts/MoneyCollectControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyCollectControl : MonoBehaviour
+{
+    public GameObject gameManeger;
+    public float size;
+    public int collectSize;
+    void Start()
+    {
+        collectSize = 0;
+        gameManeger = GameObject.Find("GameManeger");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        collectSize = gameManeger.GetComponent<GameManeger>().collectSize;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Collect");
+        if (other.gameObject.tag == "Player")
+        {
+            if (gameManeger.GetComponent<GameManeger>().objTemp != null)
+            {
+                gameManeger.GetComponent<GameManeger>().PushStack(this.gameObject);
+                gameObject.transform.SetParent(gameManeger.GetComponent<GameManeger>().collectObj.transform);
+                gameObject.transform.localScale = gameManeger.GetComponent<GameManeger>().objTemp.transform.localScale;
+                gameObject.transform.position = (Vector3.up * size) + gameManeger.GetComponent<GameManeger>().objTemp.transform.position;
+                gameObject.transform.rotation = gameManeger.GetComponent<GameManeger>().objTemp.transform.rotation;
+                gameObject.GetComponent<BoxCollider>().enabled = false;
+                gameManeger.GetComponent<GameManeger>().objTemp = gameObject;
+                
+            }
+        }
+    }
+}
