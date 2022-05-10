85949dfbf834738e3a9416fcf7985cb61d74c09a
Zekeriyaozden <zekeriyaozden@icloud.com>
2022-05-10 15:53:58 +0300

    UIController

---
+++ Assets/Scripts/UpgradeAreaUIController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeAreaUIController : MonoBehaviour
+{
+    public GameObject UI;
+    public GameObject gObj;
+    private bool flag;
+    void Start()
+    {
+        flag = true;
+    }
+    void Update()
+    {
+        if (flag)
+        {
+            
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gObj = other.gameObject;
+            flag = true;
+            //gObj.GetComponent<PlayerController>().moneyCountPlayer;
+            UI.gameObject.SetActive(true);
+            
+        }
+    }
+}
