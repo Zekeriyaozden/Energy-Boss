cbab2d3b1620225ccd98093caa08a9c07536aeac
Zekeriyaozden <zekeriyaozden@icloud.com>
2022-05-17 15:38:57 +0300

    Tranform

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -7,23 +7,63 @@ public class AIController : MonoBehaviour
 {
     private NavMeshAgent navMesh;
     public List<GameObject> objList = new List<GameObject>();
+    private GameObject destObj;
+    public int stackSize;
+    public List<GameObject> electric = new List<GameObject>();
+    private GameObject destElect;
     public int maxStackSize;
     public float speed;
+    public bool isWorking;
+    private bool objDist;
     void Start()
     {
+        isWorking = false;
         navMesh = gameObject.GetComponent<NavMeshAgent>();
     }
 
-
+    void swapTarget(bool flag)
+    {
+        if (flag)
+        {
+            objDist = false;
+            float temp = Vector3.Distance(gameObject.transform.position , electric[0].transform.position);
+            for (int i = 0; i < electric.Capacity; i++)
+            {
+                if (electric[i]) ;
+            }
+            
+            
+        }
+        else
+        {
+            
+            
+        }
+    }
+    
     void Update()
     {
-        for (int i = 0; i < objList.Capacity; i++)
+        Debug.Log(Vector3.Distance(gameObject.transform.position , objList[0].gameObject.transform.position));
+        //distance<2;
+        if (Vector3.Distance(gameObject.transform.position, objList[0].gameObject.transform.position) < 2f)
         {
-            if (objList[i].gameObject != null)
+            if (stackSize == 0)
             {
-                navMesh.destination = objList[i].gameObject.transform.position;
+                if (objDist)
+                {
+                    swapTarget(true);
+                }
             }
         }
-        //navMesh.destination = gameObj.transform.position;
+        if (objDist)
+        {
+            for (int i = 0; i < objList.Capacity; i++)
+            {
+                Debug.Log(objList.Capacity);
+           
+                navMesh.destination = objList[0].gameObject.transform.position;
+            
+            }
+        }
     }
 }
