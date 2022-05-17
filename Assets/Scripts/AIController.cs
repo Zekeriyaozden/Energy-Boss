4203a758ca419b74f8356cd8632c8af7c3b00c4c
Zekeriyaozden <zekeriyaozden@icloud.com>
2022-05-17 14:13:43 +0300

    AIController

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour
+{
+    private NavMeshAgent navMesh;
+    public List<GameObject> objList = new List<GameObject>();
+    public int maxStackSize;
+    public float speed;
+    void Start()
+    {
+        navMesh = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        for (int i = 0; i < objList.Capacity; i++)
+        {
+            if (objList[i].gameObject != null)
+            {
+                navMesh.destination = objList[i].gameObject.transform.position;
+            }
+        }
+        //navMesh.destination = gameObj.transform.position;
+    }
+}
