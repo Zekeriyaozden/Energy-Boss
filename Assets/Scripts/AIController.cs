4b03ddf0afbcf6075d2d39daa35b56f4a07163f4
Zekeriyaozden <zekeriyaozden@icloud.com>
2022-05-19 17:23:37 +0300

    AI

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -1,27 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AIController : MonoBehaviour
 {
+    public Stack<GameObject> moneyStack = new Stack<GameObject>();
+    public GameObject referanceMoney;
+    public float moneyPopSpeed;
+    public bool onTrigger;
     private NavMeshAgent navMesh;
     public List<GameObject> objList = new List<GameObject>();
     private GameObject destObj;
     public int stackSize;
+    private float stackSpawnWait;
     public List<GameObject> electric = new List<GameObject>();
     private GameObject destElect;
     public int maxStackSize;
     public float speed;
     public bool isWorking;
-    private bool objDist;
+    public bool objDist;
+    public GameObject tempElectricObj;
     void Start()
     {
         isWorking = false;
         navMesh = gameObject.GetComponent<NavMeshAgent>();
         navMesh.destination = electric[0].transform.GetChild(4).transform.position;
-        Debug.Log(electric[0].transform.GetChild(4).name);
         objDist = true;
+        stackSpawnWait = GameObject.Find("GameManeger").GetComponent<GameManeger>().stackWait;
     }
 
     void swapTarget(bool flag)
@@ -50,29 +57,157 @@ public class AIController : MonoBehaviour
             
         }
     }
+
+    void findCloserElectric()
+    {
+        float temp = Vector3.Distance(gameObject.transform.position,
+            electric[0].transform.position);
+        int flag;
+        flag = 0;
+        
+        for (int i = 0; i < electric.Count; i++)
+        {
+           // Debug.Log(electric[i].transform.GetChild(4).name + " -- name");
+            if (temp > Vector3.Distance(gameObject.transform.position,
+                electric[i].transform.position))
+            {
+                flag = i;
+                temp = Vector3.Distance(gameObject.transform.position,
+                    electric[i].transform.position);
+            }
+        }
+
+        navMesh.destination = electric[flag].transform.position;
+        if (Vector3.Distance(gameObject.transform.position, electric[flag].transform.position) < 2f)
+        {
+            
+            if (stackSize == 0)
+            {
+                swapTarget(true);
+            }
+        }
+    }
+
+    IEnumerator toObjC()
+    {
+        while (true)
+        {
+            navMesh.destination = objList[0].gameObject.transform.position;
+            if (stackSize > 0)
+            {
+                yield return new WaitForSeconds(moneyPopSpeed);
+                MoneySpend(objList[0].gameObject.transform);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        objDist = true;
+
+
+
+    }
+    
+    private void toObj()
+    {
+        navMesh.destination = objList[0].gameObject.transform.position;
+    }
+    
+    public void MoneySpend(Transform target)
+    {
+        if (stackSize > 0)
+        {
+            GameObject obj = moneyStack.Pop();
+            obj.gameObject.transform.SetParent(null);
+            obj.AddComponent<MoneyController>();
+            obj.GetComponent<MoneyController>().target = target;
+            obj.GetComponent<MoneyController>().spendSpeed = GameObject.Find("GameManeger").GetComponent<GameManeger>().spendSpeed;
+        }
+    }
+    
     
     void Update()
     {
-        navMesh.destination = electric[0].transform.GetChild(4).transform.position;
-        //Debug.Log(Vector3.Distance(gameObject.transform.position , objList[0].gameObject.transform.position));
-        //distance<2;
-        if (Vector3.Distance(gameObject.transform.position, objList[0].gameObject.transform.position) < 2f)
-        {
-            if (stackSize == 0)
-            {
-                if (objDist)
-                {
-                    swapTarget(true);
-                }
-            }
-        }
+        stackSize = moneyStack.Count;
         if (objDist)
         {
-            for (int i = 0; i < objList.Capacity; i++)
+            findCloserElectric();
+        }
+
+        if (!objDist)
+        {
+            toObj();
+        }
+    }
+    
+    IEnumerator moneyCollect(GameObject Area)
+    {
+        int stackTemp = Area.gameObject.transform.childCount;
+        while (true)
+        {
+            stackTemp = Area.gameObject.transform.childCount;
+            Debug.Log(Area.gameObject.transform.childCount);
+            if (stackTemp > 0 && maxStackSize > stackSize)
             {
-                Debug.Log(objList.Capacity);
-                //navMesh.destination = objList[0].gameObject.transform.position;
+                GameObject gm = Area.transform.GetChild(stackTemp-1).gameObject;
+                moneyStack.Push(gm);
+                gm.transform.SetParent(gameObject.transform.GetChild(2));
+                gm.transform.rotation = referanceMoney.gameObject.transform.rotation;
+                gm.GetComponent<BoxCollider>().enabled = false;
+                yield return new WaitForSeconds(stackSpawnWait);
+                gm.AddComponent<MoneyCollectToAI>();
+                gm.GetComponent<MoneyCollectToAI>().referanceObj = referanceMoney;
+                gm.GetComponent<MoneyCollectToAI>().collectSize = stackSize;
             }
+            else
+            {
+                yield return new WaitForSeconds(stackSpawnWait);
+            }
+
+            if (stackTemp == maxStackSize)
+            {
+                objDist = false;
+                break;
+            }
+
+        }
+        if (stackSize == maxStackSize)
+        {
+            onTrigger = false;
+            
+        }
+        
+        
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "moneyParent")
+        {
+            StartCoroutine(moneyCollect(other.gameObject));
+            onTrigger = true;
+        }
+
+        if (other.gameObject.tag == "Upgrade")
+        {
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "moneyParent")
+        {
+            onTrigger = false;
+        }
+
+        if (other.gameObject.tag == "Upgrade")
+        {
+            
         }
     }
 }
