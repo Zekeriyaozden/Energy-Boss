2af7e16b618b1f3716557bfed1b30aa3e6465b08
Zekeriyaozden <zekeriyaozden@icloud.com>
2022-06-13 22:11:23 +0300

    SDK

--- Assets/GAInitializer.cs
+++ Assets/GAInitializer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using GameAnalyticsSDK;
 using UnityEngine;
 
 public class GAInitializer : MonoBehaviour
@@ -7,7 +8,7 @@ public class GAInitializer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameAnalytics.Initialize();
     }
 
     // Update is called once per frame
