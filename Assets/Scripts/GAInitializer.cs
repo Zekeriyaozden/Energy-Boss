b55669be2170beb546a3237999af912bc33947c6
Zekeriyaozden <zekeriyaozden@icloud.com>
2022-06-13 22:47:02 +0300

    Revert "SDK"
    This reverts commit 2af7e16b618b1f3716557bfed1b30aa3e6465b08.

--- Assets/Scripts/GAInitializer.cs
+++ Assets/Scripts/GAInitializer.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using GameAnalyticsSDK;
 using UnityEngine;
 
 public class GAInitializer : MonoBehaviour
@@ -8,7 +7,7 @@ public class GAInitializer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameAnalytics.Initialize();
+        
     }
 
     // Update is called once per frame
